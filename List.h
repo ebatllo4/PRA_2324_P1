856fcf2bc70ac0e2b1337ed6d3e881d8fb6d02d8
Elsa Bataller Llorens <ebatllo24@gmail.com>
2023-09-29 21:21:48 +0200

    Añadida interfaz genérica List

--- List.h
+++ List.h
@@ -4,7 +4,16 @@
 template <typename T> 
 class List {
     public:
-        // ... aquí los métodos virtuales puros
+	virtual void insert(int pos, T e) = 0;
+	virtual void append(T e) = 0;
+	virtual void prepend(T e) = 0;
+	virtual T remove(int pos) =0;
+	virtual T get(int pos) = 0;
+	virtual int search(T e) = 0;
+	virtual bool empty() = 0;
+	virtual int size() =0t commit -m "Añadida interfaz genérica List";
+
+    	    // ... aquí los métodos virtuales puros
 };
 
 #endif
